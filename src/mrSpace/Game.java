99c4527172f6ad0ab42b873a9d5991a0a44fa4b2
FredXe <freddy115910810@gmail.com>
2022-06-12 12:49:21 +0800

    fred

--- src/mrSpace/Game.java
+++ src/mrSpace/Game.java
@@ -4,24 +4,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-class Game extends JFrame{
-	
+class Game extends JFrame {
+
 	BarrierOperation br = new BarrierOperation();
-	Image background = new ImageIcon("src/backcolor.png").getImage();
-	
-	Game()
-	{
+	Image background = new ImageIcon("src/background_color.png").getImage();
+
+	Game() {
 		this.setTitle("Mr.Space");
-		this.setSize(600,800);
+		this.setSize(600, 800);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
-	
+
 	@Override
-	public void paint(Graphics g)
-	{
+	public void paint(Graphics g) {
 		Graphics2D g2D = (Graphics2D) g;
-		g2D.drawImage(background,0,0,null);
-		g2D.drawImage(br.getter(),0,0,null);
+		g2D.drawImage(background, 0, 0, null);
+		g2D.drawImage(br.getter(), 0, 0, null);
 	}
 }
